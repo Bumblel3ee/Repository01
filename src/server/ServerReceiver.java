9803ee7fc1260e5c17d6e3dea7f9d7f3ad85538b
student <student@M11035>
2018-01-30 14:41:06 +0900

    14:41

--- src/server/ServerReceiver.java
+++ src/server/ServerReceiver.java
@@ -9,7 +9,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class ServerReceiver {
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
 		Socket socket = null;
