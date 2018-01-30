d71fc816440021fbd9794a59fd93891eb2f85f2b
student <student@M11035>
2018-01-30 14:38:15 +0900

    14:39

--- src/client/ClientSender.java
+++ src/client/ClientSender.java
@@ -1,7 +1,42 @@
-package tcpip02;
+package client;
 
-public class Client {
-	public static void main (String [] args) {
-		System.out.println("hi");
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class ClientSender {
+	public static void main(String[] args) {
+
+		String ip = "70.12.111.130";
+		int port = 7777;
+		Socket socket = null;
+		OutputStream os = null;
+		OutputStreamWriter osw = null;
+		try {
+			System.out.println("Hello World.");
+			socket = new Socket(ip, port);
+			System.out.println("Connection okay...");
+
+			// send data
+			os = socket.getOutputStream();
+			osw = new OutputStreamWriter(os);// for korean and etc
+			osw.write("hi ya~");
+			System.out.println("sned Complete");
+		} catch (IOException e) {
+ 
+			e.printStackTrace();
+		} finally {
+			try {
+				// in FM way the exceptions supposed to be handled separately
+				//
+				osw.close();
+				os.close();
+				socket.close();
+			} catch (IOException e) {
+	 
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/client/Server.java
+++
@@ -1,8 +0,0 @@
-package tcpip02;
-
-public class Server {
-	public static void main(String [] args) {
-		int a = 10;
-		int b = 20;
-	}
-}
