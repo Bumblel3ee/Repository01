acf74d3915a5f3a5c93444e7b5e67b886680f519
student <student@M11035>
2018-01-31 13:37:04 +0900

    31.01.2018 to Rim

---
+++ src/server/Server.java
@@ -0,0 +1,10 @@
+package server;
+
+public class Server {
+	public static void main(String [] args) {
+		/*ServerChat serverChat= null;
+		int port;
+		port = 8888;
+		serverChat = new ServerChat(port);*/
+	}
+}
